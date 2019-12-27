d8868b685da83124aa30812e38e7e7935c6a33be
yyh-gl <yhonda.95.gl@gmail.com>
2019-12-28 01:16:52 +0900

    CLIを起動できる最低限を実装

---
+++ cmd/proxy-switcher/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/xerrors"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "psw",
+		Usage: "Proxy Switcher for CLI",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Hello friend!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args); if err != nil {
+		log.Panic(xerrors.Errorf("CLI起動エラー: %w", err))
+	}
+}
